84db2ae4365a6ca099572ab15c09885eeaef76b9
sys <sys@sys-PC>
2017-10-05 20:00:51 +0530

    oct5 class

---
+++ Sample1/src/opps4/D.java
@@ -0,0 +1,25 @@
+package opps4;
+
+public class D extends C {
+
+	public void m3() {
+
+	}
+
+	public void m2() {
+System.out.println("Hello");
+	}
+
+	public void mm() {
+
+	}
+
+	public static void main(String[] args) {
+		/*D d = new D();
+		d.m1();*/
+		C cc=new D();
+		cc.m2();
+		cc.m1();
+	}
+
+}
