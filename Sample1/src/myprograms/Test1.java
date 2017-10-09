848db6e7344fb30f1d68c2fb34f8ad235105458c
sys <sys@sys-PC>
2017-10-09 17:58:24 +0530

    yuuyg

---
+++ Sample1/src/myprograms/Test1.java
@@ -0,0 +1,19 @@
+package myprograms;
+
+public class Test1 {
+	
+	public int ftr=10;
+	public static void t1() {
+		
+		System.out.println("hyderabad");
+	}
+
+	public static void main(String[] args) {
+		//System.out.println("this is nag");
+          
+		//Test1 rr = new Test1();
+		
+		 t1();
+	}
+
+}
---
+++ Sample1/src/myprograms/Test2.java
@@ -0,0 +1,14 @@
+package myprograms;
+
+public class Test2  {
+	
+	
+	
+	public static void main(String[] args) {
+		
+				
+		
+		
+        	}
+
+}
